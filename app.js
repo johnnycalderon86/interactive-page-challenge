5a772f057575ea3b1ab978004ee1e5aac5e11fce
Johnny <johnnycalderondeveloper@gmail.com>
2019-08-01 13:58:59 +0200

    added some comments to app.js, and fixed the slider

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-
+//--------------Slider-----------------//
 
 var slideIndex = 1;
 showSlides(slideIndex);
@@ -30,7 +30,23 @@ function showSlides(n) {
     dots[slideIndex-1].className += " active";
     
 }
+//----------------------Automatic Slide-----------------------//
+var slideIndex = 0;
+showSlides1();
 
+function showSlides1() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1} 
+  slides[slideIndex-1].style.display = "block"; 
+  setTimeout(showSlides1, 4000); // Change image every 4 seconds
+}
+
+//-----------------------Tabs----------------------------------//
 $(document).ready(function() {
     $('#tabs li a:not(:first)').addClass('inactive');
     $('.container:not(:first)').hide();
@@ -51,18 +67,4 @@ if($(this).hasClass('inactive')){ //this is the start of our condition
 }
 
 });
-
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none"; 
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1} 
-  slides[slideIndex-1].style.display = "block"; 
-  setTimeout(showSlides, 4000); // Change image every 2 seconds
-}
+//------------------image arraow keys-------------------------//
