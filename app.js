d0ec8f321347a36051917bbae9091f023b655ba5
Johnny <johnnycalderondeveloper@gmail.com>
2019-08-01 16:15:06 +0200

    numbers interactive

--- app.js
+++ app.js
@@ -35,15 +35,15 @@ var slideIndex = 0;
 showSlides1();
 
 function showSlides1() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none"; 
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1} 
-  slides[slideIndex-1].style.display = "block"; 
-  setTimeout(showSlides1, 4000); // Change image every 4 seconds
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides1, 4000); // Change image every 4 seconds
 }
 
 //-----------------------Tabs----------------------------------//
@@ -51,27 +51,36 @@ $(document).ready(function() {
     $('#tabs li a:not(:first)').addClass('inactive');
     $('.container:not(:first)').hide();
     $('#tabs li a').click(function(){
-var t = $(this).attr('href');
-$('#tabs li a').addClass('inactive');
-$(this).removeClass('inactive');
-$('.container').hide();
-$(t).fadeIn('slow');
-return false;
-
-})
-if($(this).hasClass('inactive')){ //this is the start of our condition
-    $('#tabs li a').addClass('inactive');
-    $(this).removeClass('inactive');
-    $('.container').hide();
-    $(t).fadeIn('slow');
-}
-
+        var t = $(this).attr('href');
+        $('#tabs li a').addClass('inactive');
+        $(this).removeClass('inactive');
+        $('.container').hide();
+        $(t).fadeIn('slow');
+        return false;
+        
+    })
+    if($(this).hasClass('inactive')){ //this is the start of our condition
+        $('#tabs li a').addClass('inactive');
+        $(this).removeClass('inactive');
+        $('.container').hide();
+        $(t).fadeIn('slow');
+    }
+    
 });
-//------------------image arraow keys-------------------------//
 
+//-----------------making the numbers interactive-----------------//
 
-
-
-
-
-
+$(function() {
+    function count($this){
+        var current = parseInt($this.html(), 10);
+        $this.html(++current);
+        if(current !== $this.data('count')){
+            setTimeout(function(){count($this)}, 0.01);
+        }
+    }        
+  $("span").each(function() {
+      $(this).data('count', parseInt($(this).html(), 10));
+      $(this).html('0');
+      count($(this));
+  });
+});
